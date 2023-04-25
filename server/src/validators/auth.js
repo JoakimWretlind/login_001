956fe76e6f5f4e56b824ee11b43d18bea7f1be74
JoakimWretlind <joakim.wretlind@gmail.com>
2023-04-25 22:33:02 +0200

    set up some validators

---
+++ server/src/validators/auth.js
@@ -0,0 +1,28 @@
+const { check } = require("express-validator");
+const db = require("../db");
+
+// check password - has it the correct length
+const password = check("password")
+  .isLength({ min: 6, max: 15 })
+  .withMessage("Password has to be between 6 and 15 characters.");
+
+// check email - is the email-format an email
+const email = check("email")
+  .isEmail()
+  .withMessage("Please provide a valid email.");
+
+// check if email exists - every user must have a unique email
+const emailExists = check("email").custom(async (value) => {
+  const { rows } = await db.query("SELECT * FROM users WHERE email = $1", [
+    value,
+  ]);
+  if (rows.length) {
+    throw new Error("Email already exists.");
+  }
+});
+
+module.exports = {
+  registerValidation: [email, password, emailExists],
+};
+
+// $1 -> the first item of the array
