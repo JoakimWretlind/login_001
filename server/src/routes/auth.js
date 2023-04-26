71b5951ee804c71a0ae4f750ad5562ff4ac19536
JoakimWretlind <joakim.wretlind@gmail.com>
2023-04-26 12:59:10 +0200

    created login validation

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -4,10 +4,11 @@ const { getUsers, register } = require("../controllers/auth");
 const {
   validationMiddleware,
 } = require("../middlewares/validations-middleware");
-const { registerValidation } = require("../validators/auth");
+const { registerValidation, loginValidation } = require("../validators/auth");
 const router = Router();
 
 router.get("/get-users", getUsers); // from controllers/auth
 router.post("/register", registerValidation, validationMiddleware, register); // from validators/auth & controllers/auth
+router.post("/login", loginValidation, validationMiddleware); // from validators/auth/
 
 module.exports = router;
--- server/src/validators/auth.js
+++ server/src/validators/auth.js
@@ -1,5 +1,6 @@
 const { check } = require("express-validator");
 const db = require("../db");
+const { compare } = require("bcrypt");
 
 // check password - has it the correct length
 const password = check("password")
@@ -18,10 +19,29 @@ const emailExists = check("email").custom(async (value) => {
   ]);
 
   if (rows.length) {
-    throw new Error("Email already exists.");
+    throw new Error("Email already exist.");
   }
+});
+
+// login validation
+const loginFieldsCheck = check("email").custom(async (value, { req }) => {
+  const user = await db.query("SELECT * from users WHERE email = $1", [value]);
+
+  if (!user.rows.length) {
+    throw new Error("Email does not exist.");
+  }
+
+  // the compare function comes from bcrypt
+  const validPassword = await compare(req.body.password, user.rows[0].password);
+
+  if (!validPassword) {
+    throw new Error("Wrong password");
+  }
+
+  req.user = user.rows[0];
 });
 
 module.exports = {
   registerValidation: [email, password, emailExists],
+  loginValidation: [loginFieldsCheck],
 };
