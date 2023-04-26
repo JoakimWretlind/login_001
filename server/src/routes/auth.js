5445b803f56df758a0e08683915aa4127d97eab7
JoakimWretlind <joakim.wretlind@gmail.com>
2023-04-26 11:08:21 +0200

    set up validation

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,11 +1,14 @@
 // routes related to auth
 const { Router } = require("express");
 const { getUsers } = require("../controllers/auth");
+const {
+  validationMiddleware,
+} = require("../middlewares/validations-middleware");
 const { registerValidation } = require("../validators/auth");
 const router = Router();
 
 // getUser is a function from controllers/auth
 router.get("/get-users", getUsers); // from controllers/auth
-router.post("/register", registerValidation); // from validators/auth
+router.post("/register", registerValidation, validationMiddleware); // from validators/auth
 
 module.exports = router;
