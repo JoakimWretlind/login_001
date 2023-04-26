d46accddc681a43a22147c0e0e84522a8b5f9e35
JoakimWretlind <joakim.wretlind@gmail.com>
2023-04-26 11:40:09 +0200

    fixed registration and get user without hashed password

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,18 +1,29 @@
 const db = require("../db");
+const { hash } = require("bcrypt");
 
 exports.getUsers = async (req, res) => {
   try {
-    const { rows } = await db.query("SELECT * FROM users");
-    console.log(rows);
+    const { rows } = await db.query("select user_id, email from users");
   } catch (error) {
     console.log(error.message);
   }
 };
 
 exports.register = async (req, res) => {
+  const { email, password } = req.body;
   try {
-    console.log("validation passed");
+    const hashedPassword = await hash(password, 10);
+    await db.query("INSERT INTO users(email, password) VALUES($1, $2)", [
+      email,
+      hashedPassword,
+    ]);
+    return res.status(201).json({
+      success: true,
+      message: "Registration was successfull",
+    });
   } catch (error) {
-    console.log(error.message);
+    return res.status(500).json({
+      error: error.message,
+    });
   }
 };
